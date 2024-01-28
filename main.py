11af6d1ee129cf71841d301380a9caec1f6e302e
Beata Banduch-Macioł <beata.banduch@comarch.com>
2024-01-28 12:31:53 +0100

    s

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from flask import Flask
 from flask_restful import Resource, Api
+import cv2
+
 app = Flask(__name__)
 api = Api(app)
+
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+
+class PeopleCounter(Resource):
+    def get(self):
+        img = cv2.imread('foto.jpg')
+        boxes, weights = hog.detectMultiScale(img, winStride=(1, 1))
+        return {'count': len(boxes)}
 
 
 class HelloWorld(Resource):
@@ -9,13 +21,8 @@ class HelloWorld(Resource):
         return {'hello': 'world'}
 
 
-class HelloWorld2(Resource):
-    def get(self):
-        return {'hello': 'world2'}
-
-
 api.add_resource(HelloWorld,'/test')
-api.add_resource(HelloWorld, '/test2')
+api.add_resource(PeopleCounter, '/test3')
 
 if __name__ == '__main__':
     app.run(debug=True)

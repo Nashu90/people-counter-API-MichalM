278efa40b7384dabead4e9d15f200ade5d6becf9
Beata Banduch-Macioł <beata.banduch@comarch.com>
2024-01-28 10:32:10 +0100

    adding actions

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 from flask import Flask
 from flask_restful import Resource, Api
-
 app = Flask(__name__)
 api = Api(app)
 
+
 class HelloWorld(Resource):
     def get(self):
-        return {'hello' : 'world'}
+        return {'hello': 'world'}
+
 
 class HelloWorld2(Resource):
     def get(self):
-        return {'hello' : 'world2'}
+        return {'hello': 'world2'}
 
 
-api.add_resource(HelloWorld,'/test')
-api.add_resource(HelloWorld,'/test2')
+api.add_resource(HelloWorld, '/test')
+api.add_resource(HelloWorld, '/test2')
 
 if __name__ == '__main__':
     app.run(debug=True)
